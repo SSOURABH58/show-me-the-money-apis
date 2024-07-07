67f27237c83e6bd7830ac4be17bb9c4fbe8bf78c
Sourabh Soni <ssourabh025@gmail.com>
2024-07-08 02:24:53 +0530

    added all test cases

--- src/balance-sheet/balance-sheet.controller.spec.ts
+++ src/balance-sheet/balance-sheet.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BalanceSheetController } from './balance-sheet.controller';
+import { HttpModule } from '@nestjs/axios';
+import { BalanceSheetService } from './balance-sheet.service';
 
 describe('BalanceSheetController', () => {
   let controller: BalanceSheetController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       controllers: [BalanceSheetController],
+      providers: [BalanceSheetService],
     }).compile();
 
     controller = module.get<BalanceSheetController>(BalanceSheetController);
@@ -14,5 +18,14 @@ describe('BalanceSheetController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should have a Get method', () => {
+    expect(controller.getBalanceSheet).toBeDefined();
+  });
+
+  it('should return balance sheet', async () => {
+    const data = controller.getBalanceSheet();
+    expect(data).toBeDefined();
   });
 });
--- src/balance-sheet/balance-sheet.service.spec.ts
+++ src/balance-sheet/balance-sheet.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BalanceSheetService } from './balance-sheet.service';
+import { HttpModule } from '@nestjs/axios';
 
 describe('BalanceSheetService', () => {
   let service: BalanceSheetService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [BalanceSheetService],
     }).compile();
 
@@ -14,5 +16,14 @@ describe('BalanceSheetService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should have getBalanceSheet method', () => {
+    expect(service.getBalanceSheet).toBeDefined();
+  });
+
+  it('should return balance sheet', async () => {
+    const data = service.getBalanceSheet();
+    expect(data).toBeDefined();
   });
 });
